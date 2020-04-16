8bb9c9e8795e85e230b23598f5bb207129f89850
Laura Gomez <laura@elenagoto.ch>
2020-04-16 11:18:02 +0200

    Add ActiveRecord scopes to replace inline usages in controllers

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -1,10 +1,17 @@
 class Tip < ApplicationRecord
-  belongs_to :user
-  has_many   :comments
-  
-  has_and_belongs_to_many :users
-
+  # Validation rules
   validates :title, presence: true,
                     length: { maximum: 100 }
   validates :body,  presence: true
+
+  # Relationships
+  belongs_to :user
+  has_many   :comments
+  has_and_belongs_to_many :users
+
+  # Scopes
+  scope :most_recent_active, -> { order(updated_at: :desc).limit(4) }
+  scope :title_contains, ->(term) { where('title LIKE ?', "%#{term}%") }
+  scope :body_contains, ->(term) { where('body LIKE ?', "%#{term}%") }
+  scope :search, ->(search_term) { title_contains(search_term).or(body_contains(search_term)) }
 end
