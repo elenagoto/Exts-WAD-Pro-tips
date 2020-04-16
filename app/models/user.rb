ff93784bb7fe43af2d5cf439ca9f1535f4fabe5d
Laura Gomez <laura@elenagoto.ch>
2020-04-16 11:57:53 +0200

    Add paginates_per to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  has_many :tips
-  has_many :comments
-
-  has_and_belongs_to_many :favourites, class_name: Tip.name
-
+  # Validation rules
   validates :email, presence: true,
                     uniqueness: true
+
+  # Relationships
+  has_many :tips
+  has_many :comments
+  has_and_belongs_to_many :favourites, class_name: Tip.name
+
+  # Pagination
+  paginates_per 6
 end
