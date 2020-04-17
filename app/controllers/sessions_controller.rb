900cd65c21adc8b0bed961b45ef03987fae0da77
Laura Gomez <laura@elenagoto.ch>
2020-04-17 20:35:15 +0200

    Implement log in mechanism

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,18 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
+    user = User.find_by(email: params[:email].downcase)
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:alert] = 'Email or password were invalid.  Please try again'
+      render 'new'
+      @user = User.new
+    end
   end
 
   def destroy
