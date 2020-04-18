cc18ff952d8bee54c96a70096f3c73b399942b28
Laura Gomez <laura@elenagoto.ch>
2020-04-18 20:53:12 +0200

    Create 'can_edit_user?' helper method and make us in user_card

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -6,4 +6,12 @@ module RolesHelper
     else false
     end
   end
+
+  def can_edit_user?(user)
+    case current_user.role
+    when 'admin' then true
+    when 'registered' then user == current_user
+    else false
+    end
+  end
 end
