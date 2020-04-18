fd418f19351163b9b1d225bb15d31b191e32da7b
Laura Gomez <laura@elenagoto.ch>
2020-04-18 19:11:19 +0200

    Add role attribute to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200417173627) do
+ActiveRecord::Schema.define(version: 20200418171039) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20200417173627) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "role"
   end
 
 end
