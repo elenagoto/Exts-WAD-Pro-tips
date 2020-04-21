1bc4e75fc40c38fa3e8c458cfd383b77387e29c5
Laura Gomez <laura@elenagoto.ch>
2020-04-21 18:24:33 +0200

    Implement delete User in user_card only for admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
   # Before actions
   before_action :ensure_authenticated,   only: [:index, :edit, :update, :destroy]
   before_action :set_user,               only: [:show, :edit, :update, :destroy]
-  before_action :authorize_to_edit_user, only: [:edit, :update, :destroy]
-  before_action :ensure_admin,           only: [:index]
+  before_action :authorize_to_edit_user, only: [:edit, :update]
+  before_action :ensure_admin,           only: [:index, :destroy]
 
   # Actions
   def index
