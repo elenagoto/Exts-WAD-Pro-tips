6dad385ec1644a780918edea5a7c50fb32656f62
Laura Gomez <laura@elenagoto.ch>
2020-04-24 17:57:46 +0200

    Implement destroy using asynchronous Ajax request

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var Users;
+
+Users = {};
+
+Users.removeUserCard = function(userId) {
+  document.getElementById('user-' + userId).parentElement.remove();
+};
