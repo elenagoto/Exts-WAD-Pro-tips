5436e1fbfdf3f15ac32b615a69a9618b73d02756
Laura Gomez <laura@elenagoto.ch>
2020-04-26 16:13:20 +0200

    Implement Delete method via Ajax using JS and rails-ujs

--- app/assets/javascripts/tips.js
+++ app/assets/javascripts/tips.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var Tips;
+
+Tips = {};
+
+Tips.removeTipCard = function(tipId) {
+  document.getElementById('tip-' + tipId).parentElement.remove();
+};
+
+Tips.addNotice = function() {
+  document.getElementById('notice').textContent = 'Tip was successfully destroyed.';
+};
