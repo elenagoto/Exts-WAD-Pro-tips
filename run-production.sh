dc4021a9cf3e694b165aa87127906615d525db7f
Laura Gomez <laura@elenagoto.ch>
2020-04-27 16:28:56 +0200

    Add run-production script to repo

---
+++ run-production.sh
@@ -0,0 +1,4 @@
+export SECRET_KEY_BASE=355c9c133770c7aebfc0f2f3198099eccf95449bcd1ddfdd007c40ae968ff45b1c9d6aaaff0c96e82e977247b6ee4b558db8487d4a912849d1a5beb74e798b09
+export RAILS_SERVE_STATIC_FILES=true
+
+rails s -e production
